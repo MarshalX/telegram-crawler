d49d922412b89f223277f3022e83bf525b2e06d6
GitHub Action <github-action@users.noreply.github.com>
2022-04-05 14:48:22 +0000

    Update content of files

--- data/telegram.org/js/telegram-webview.js
+++ data/telegram.org/js/telegram-webview.js
@@ -301,8 +301,7 @@
 
     onEvent('main_button_pressed', onMainButtonPressed);
 
-    var debugBtn = null, debugBtnStyle = {},
-        fixedContainer = null;
+    var debugBtn = null, debugBtnStyle = {};
     if (initParams.tgDebug) {
       debugBtn = document.createElement('tg-main-button');
       debugBtnStyle = {
@@ -329,7 +328,6 @@
         document.removeEventListener('DOMContentLoaded', onDomLoaded);
         document.body.appendChild(debugBtn);
         debugBtn.addEventListener('click', onMainButtonPressed, false);
-        fixedContainer = document.getElementById('tg-fixed-container');
       });
     }
 
@@ -356,6 +354,8 @@
         debugBtn.style.backgroundImage = isProgressVisible ? "url('data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20viewport%3D%220%200%2048%2048%22%20width%3D%2248px%22%20height%3D%2248px%22%3E%3Ccircle%20cx%3D%2250%25%22%20cy%3D%2250%25%22%20stroke%3D%22%23fff%22%20stroke-width%3D%222.25%22%20stroke-linecap%3D%22round%22%20fill%3D%22none%22%20stroke-dashoffset%3D%22106%22%20r%3D%229%22%20stroke-dasharray%3D%2256.52%22%20rotate%3D%22-90%22%3E%3Canimate%20attributeName%3D%22stroke-dashoffset%22%20attributeType%3D%22XML%22%20dur%3D%22360s%22%20from%3D%220%22%20to%3D%2212500%22%20repeatCount%3D%22indefinite%22%3E%3C%2Fanimate%3E%3CanimateTransform%20attributeName%3D%22transform%22%20attributeType%3D%22XML%22%20type%3D%22rotate%22%20dur%3D%221s%22%20from%3D%22-90%2024%2024%22%20to%3D%22630%2024%2024%22%20repeatCount%3D%22indefinite%22%3E%3C%2FanimateTransform%3E%3C%2Fcircle%3E%3C%2Fsvg%3E')" : 'none';
         debugBtn.style.backgroundColor = color;
         debugBtn.style.color = text_color;
+
+        var fixedContainer = document.getElementById('tg-fixed-container');
         if (fixedContainer) {
           fixedContainer.style.top = isVisible ? '-48px' : '0';
         }
