c9f59f1315c1a6fcbb624df9424fe05b77d21c74
GitHub Action <github-action@users.noreply.github.com>
2024-01-01 13:33:42 +0000

    Update content of files

--- data/web/fragment.com/js/auction.js
+++ data/web/fragment.com/js/auction.js
@@ -1048,7 +1048,9 @@ var Assets = {
         username: username,
         auction: true
       }, function(result) {
-        if (result.confirm_message) {
+        if (result.error) {
+          return showAlert(result.error);
+        } else if (result.confirm_message) {
           showConfirm(result.confirm_message, function() {
             doPutToAuction();
           }, result.confirm_button);
@@ -1148,7 +1150,9 @@ var Assets = {
         username: username,
         auction: false
       }, function(result) {
-        if (result.confirm_message) {
+        if (result.error) {
+          return showAlert(result.error);
+        } else if (result.confirm_message) {
           showConfirm(result.confirm_message, function() {
             doSellUsername();
           }, result.confirm_button);
